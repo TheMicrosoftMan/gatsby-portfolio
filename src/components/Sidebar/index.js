2387e2001b79a28266e775803186d565fc4fe607
TheMicrosoftMan <yurazelenko@windowslive.com>
2019-10-21 14:40:24 +0300

    Add sidebar opportunity to open up

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,34 +1,73 @@
-import React from "react"
-import { Link } from "gatsby"
+import React, { useState } from "react";
+import { Link } from "gatsby";
+import { CSSTransition } from "react-transition-group";
 
-const Sidebar = ({ siteTitle }) => (
-  <aside className="sidebar">
-    <Link to="/">
-      <span className="sidebar__icon">
-        <i className="mi mi-Home" />
-      </span>
-    </Link>
-    <Link to="/blog">
-      <span className="sidebar__icon">
-        <i className="mi mi-Dictionary" />
-      </span>
-    </Link>
-    <Link to="/skills">
-      <span className="sidebar__icon">
-        <i className="mi mi-Code" />
-      </span>
-    </Link>
-    <Link to="/repos">
-      <span className="sidebar__icon">
-        <i className="mi mi-github" />
-      </span>
-    </Link>
-    <Link to="/about">
-      <span className="sidebar__icon">
-        <i className="mi mi-ContactInfo" />
-      </span>
-    </Link>
-  </aside>
-)
+const Sidebar = () => {
+  const [sidebarState, setSidebarState] = useState(false);
 
-export default Sidebar
+  const sidebarItems = [
+    {
+      icon: <i className="mi mi-Home" />,
+      name: "Home",
+      link: "/"
+    },
+    {
+      icon: <i className="mi mi-Dictionary" />,
+      name: "Blog",
+      link: "/blog"
+    },
+    {
+      icon: <i className="mi mi-Code" />,
+      name: "Projects",
+      link: "/skills"
+    },
+    {
+      icon: <i className="mi mi-github" />,
+      name: "GitHub",
+      link: "/repos"
+    },
+    {
+      icon: <i className="mi mi-ContactInfo" />,
+      name: "About",
+      link: "/about"
+    }
+  ];
+
+  return (
+    <aside className={`sidebar${sidebarState ? " sidebar-open" : ""}`}>
+      <div
+        className="sidebar__item"
+        onClick={() => setSidebarState(!sidebarState)}
+      >
+        <i className="mi mi-GlobalNavigationButton" />
+        <CSSTransition
+          in={sidebarState}
+          timeout={150}
+          classNames="opacity-animation"
+          unmountOnExit
+        >
+          <span className="sidebar__item_title">Menu</span>
+        </CSSTransition>
+      </div>
+      {sidebarItems.map((item, index) => {
+        return (
+          <Link to={item.link}>
+            <div className="sidebar__item">
+              {item.icon}
+              <CSSTransition
+                in={sidebarState}
+                timeout={150}
+                classNames="opacity-animation"
+                unmountOnExit
+              >
+                <span className="sidebar__item_name">{item.name}</span>
+              </CSSTransition>
+            </div>
+          </Link>
+        );
+      })}
+    </aside>
+  );
+};
+
+export default Sidebar;
